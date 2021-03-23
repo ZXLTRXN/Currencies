53f73b03f0e8f75628d485e7c991dd80a467f005
Ilya Shevtsov <madbastaz21@gmail.com>
2021-03-23 16:00:54 +0700

    realized NetworkUtils class

--- app/src/main/java/com/example/currencies/MainActivity.java
+++ app/src/main/java/com/example/currencies/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.currencies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ConvertInput;
+    private TextView ConvertRes;
+    private Button ConvertBtn;
+    private Spinner chooseCurrency;
+    private ListView CurrenciesList;
+    //private ProgressBar loadingIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConvertInput = findViewById(R.id.et_converter);
+        ConvertRes = findViewById(R.id.tv_converter);
+        ConvertBtn = findViewById(R.id.btn_converter);
+        chooseCurrency = findViewById(R.id.spinner_converter);
+        CurrenciesList = findViewById(R.id.lv_currencies);
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/currencies/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/currencies/Utils/NetworkUtils.java
@@ -1,4 +1,48 @@
 package com.example.currencies.Utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class NetworkUtils {
+    private static final String CB_DATA_URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+    public static String getResponseFromURL() throws IOException {
+        URL url = null;
+        String result;
+
+        try{
+            url = new URL(CB_DATA_URL);
+        }catch(MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            result = readString(in);
+            in.close();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }finally {
+            urlConnection.disconnect();
+        }
+        return result;
+    }
+
+    private static String readString(InputStream in) {
+        Scanner scanner = new Scanner(in);
+        scanner.useDelimiter("\\A");//\\A = начало строки, таким образом мы считаем построчно, по дефолту разделитель - пробел
+        boolean hasInput = scanner.hasNext();
+        if (hasInput) {
+            return scanner.next();
+        } else {
+            return null;
+        }
+    }
 }
